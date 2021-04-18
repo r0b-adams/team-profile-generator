13cde888213c05145b2ca9d9f41878a818bbf7aa
Robert Adams <adamsiii.robert@gmail.com>
2021-04-17 20:00:56 -0700

    added pseudocode

--- index.js
+++ index.js
@@ -3,29 +3,168 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
 const inquirer = require('inquirer');
+const fs = require('fs');
+/*
+--testing class imports--
+const testManager = new Manager("Bob", 1, "bob@bob.bob", 42);
+console.log(testManager);
 
-// testing class imports
-// const testManager = new Manager("Bob", 1, "bob@bob.bob", 42);
-// console.log(testManager);
+const testEngineer = new Engineer("Bob", 1, "bob@bob.bob", 42);
+console.log(testEngineer);
 
-// const testEngineer = new Engineer("Bob", 1, "bob@bob.bob", 42);
-// console.log(testEngineer);
+const testIntern = new Intern("Bob", 1, "bob@bob.bob", 42);
+console.log(testIntern);
 
-// const testIntern = new Intern("Bob", 1, "bob@bob.bob", 42);
-// console.log(testIntern);
+--testing inquirer--
+inquirer
+  .prompt([
+      {
+        name: "test",
+        type: "input",
+        message: "What is your quest?",
+      }
+  ])
+  .then(answers => {
+    console.log(answers);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+*/
 
-// testing inquirer
-// inquirer
-//   .prompt([
-//       {
-//         name: "test",
-//         type: "input",
-//         message: "What is your quest?",
-//       }
-//   ])
-//   .then(answers => {
-//     console.log(answers);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
+const newTeamMemberQuestions = [
+    {
+        name: 'name',
+        type: 'input',
+        message: 'modify this question based on employee type selected:',
+    },
+
+    {
+        name: 'id',
+        type: 'input',
+        message: 'Enter employee ID:',
+    },
+
+    {
+        name: 'email',
+        type: 'input',
+        message: 'Enter email:',
+        // validate: (Function) Receive the user input and answers hash. 
+        // Should return true if the value is valid, and an error message (String) otherwise. 
+        // If false is returned, a default error message is provided.
+
+        // function that takes a str as input and makes sure it contains an @ and no spaces
+    },
+
+    {
+        name: 'misc',
+        type: 'input',
+        message: 'modify this question based on employee type selected:',
+        // validate: 
+    },
+ ];
+
+ const mainMenuChoices = [
+    {
+        type: 'list',
+        message: 'add a team member?',
+        name: 'choice',
+        choices: ['add an engineer', 'add an intern', 'finish building my team',],
+      },
+ ];
+
+// PSEUDOCODE
+
+/*
+const teamArray = []; // stores team
+addManager()
+*/
+
+/*
+addManager() {
+
+  newTeamMemberQuestions[0].message = "what is the team manager’s name?"
+  newTeamMemberQuestions[3].message = "what is the team manager’s office number?"
+
+  inquire (newTeamMemberQuestions)
+      validate each
+
+  then (data)
+    make new Manager(use data properties)
+    push to teamArray
+    mainMenu();
+}
+*/
+
+/*
+mainMenu() {
+
+  inquire (mainMenuChoices)
+
+  then
+    if (add an engineer) {
+        addEngineer()
+    } else if (add an intern) {
+        addIntern()
+    } else {
+        writeHTML()
+    }
+}
+*/
+
+/*
+addEngineer() {
+
+  newTeamMemberQuestions[0].message = "what is the engineer’s name?"
+  newTeamMemberQuestions[3].message = "what is the engineer’s office number?"
+
+  inquire (newTeamMemberQuestions)
+      validate each
+
+  then
+    make new Engineer
+    push to teamArray
+    mainMenu();
+}
+*/
+
+/*
+addIntern() {
+
+  newTeamMemberQuestions[0].message = "what is the intern’s name?"
+  newTeamMemberQuestions[3].message = "what is the intern’s school?"
+
+  inquire (newTeamMemberQuestions)
+      validate each
+
+  then
+    make new Intern
+    push to teamArray
+    mainMenu();
+}
+*/
+
+/*
+writeHTML() {
+
+const teamTemplate = "";
+for each team member 
+    enter the following into a string template literal:
+        name
+        role
+          icon corresponding to role
+        id
+        email
+        office#/githubLink/school
+
+    teamTemplate += stringTemplateLiteral;
+
+place teamTemplate string into larger template
+
+note: data must be a string
+fs.writeFile('teamprofile.html', data, (err) =>
+  err ? console.error(err) : console.log('HTML successfully generated!')
+);
+
+}
+*/
