8a1838ac6d4b4a907d96145879f47c682159a162
Robert Adams <adamsiii.robert@gmail.com>
2021-04-18 15:49:42 -0700

    removed test code

--- index.js
+++ index.js
@@ -4,33 +4,6 @@ const Intern = require("./lib/Intern");
 
 const inquirer = require('inquirer');
 const fs = require('fs');
-/*
---testing class imports--
-const testManager = new Manager("Bob", 1, "bob@bob.bob", 42);
-console.log(testManager);
-
-const testEngineer = new Engineer("Bob", 1, "bob@bob.bob", 42);
-console.log(testEngineer);
-
-const testIntern = new Intern("Bob", 1, "bob@bob.bob", 42);
-console.log(testIntern);
-
---testing inquirer--
-inquirer
-  .prompt([
-      {
-        name: "test",
-        type: "input",
-        message: "What is your quest?",
-      }
-  ])
-  .then(answers => {
-    console.log(answers);
-  })
-  .catch(error => {
-    console.log(error);
-  });
-*/
 
 const newTeamMemberQuestions = [
     {
